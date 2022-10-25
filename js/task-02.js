75d50a027049e147a209ddec80ecb97f8641b526
VikaVelhan <velganvika@gmail.com>
2022-10-26 00:32:05 +0300

    task-02-js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+const listIngEl = [];
+
+for (const ingredient of ingredients) {
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent = ingredient;
+  listIngEl.push(liEl);
+}
+listEl.append(...listIngEl);
+console.log(listEl);
