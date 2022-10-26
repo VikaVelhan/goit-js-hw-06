4c3cede55dcc198b74e954e6627bc0f5a444e2ef
VikaVelhan <velganvika@gmail.com>
2022-10-26 17:43:59 +0300

    task-04-js

---
+++ js/task-04.js
@@ -0,0 +1,20 @@
+let counterValue = 0;
+const counterEl = document.querySelector("#value");
+const btnDecrement = document.querySelector('[data-action="decrement"]');
+const btnIncrement = document.querySelector('[data-action="increment"]');
+
+const onDecrementClick = () => {
+  counterValue -= 1;
+  counterEl.textContent = counterValue;
+};
+const onIncrementClick = () => {
+  counterValue += 1;
+  counterEl.textContent = counterValue;
+};
+
+btnDecrement.addEventListener("click", onDecrementClick);
+btnIncrement.addEventListener("click", onIncrementClick);
+
+console.log(counterValue);
+console.log(btnDecrement);
+console.log(btnIncrement);
