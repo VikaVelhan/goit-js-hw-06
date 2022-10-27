84630d6b936720780d0db62b0b0eb9041a7e10fe
VikaVelhan <velganvika@gmail.com>
2022-10-27 19:23:16 +0300

    task-08-js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
-event.preventDefault();
+const formEl = document.querySelector(".login-form");
+console.log(formEl);
+
+formEl.addEventListener("submit", onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert`Заповніть всі поля`;
+  } else {
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+  }
+}
