91c9b5be40110ce7fb490d02d45ba13885fdbe85
VikaVelhan <velganvika@gmail.com>
2022-10-27 21:06:25 +0300

    task-09-js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const widgetEl = document.querySelector(".widget");
+const colorEl = document.querySelector(".color");
+const btnChangeColor = document.querySelector(".change-color");
+
+btnChangeColor.addEventListener("click", onChangeColor);
+function onChangeColor() {
+  widgetEl.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = `${widgetEl.style.backgroundColor}`;
+}
